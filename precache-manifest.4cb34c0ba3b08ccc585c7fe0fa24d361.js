99dad590a4e81434c096ff2afce29bc43a39fe0c
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2019-04-17 13:02:55 -0300

    App build 2019-04-17 13:02:55-03:00

---
+++ precache-manifest.4cb34c0ba3b08ccc585c7fe0fa24d361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1915c02931145fb5814f",
+    "url": "/static/js/main.8ef67df0.chunk.js"
+  },
+  {
+    "revision": "b520e3abc04e235ba6c4",
+    "url": "/static/js/2.b0beec72.chunk.js"
+  },
+  {
+    "revision": "1915c02931145fb5814f",
+    "url": "/static/css/main.387d6c21.chunk.css"
+  },
+  {
+    "revision": "a8f42af423e59f6154e64dde091bdd28",
+    "url": "/index.html"
+  }
+];
